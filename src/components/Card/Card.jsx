c463c2a91ad6fc24efe24336ca42d1110e07d129
SuperCoder <propscode254@gmail.com>
2023-10-25 12:34:25 +0200

    list done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,21 +2,21 @@ import PropTypes from "prop-types";
 import "./Card.scss";
 import Button from "../Button/Button";
 
-const Card = (props) => {
+const Card = ({ beerItem }) => {
   return (
     <>
       <article>
         <div className="img-card-wrapper">
-          <img src={props.image_url} alt={props.name} />
+          <img src={beerItem.image_url} alt={beerItem.name} />
         </div>
         <div className="text-card-wrapper">
-          <h2>{props.name}</h2>
-          <h3>{props.tagline}</h3>
-          <p>Created by : {props.name}</p>
+          <h2>{beerItem.name}</h2>
+          <h3>{beerItem.tagline}</h3>
+          <p>Created by : {beerItem.name}</p>
           <Button
             bgColor="bg-yellow"
             element="Details"
-            href={`/beer/${props._id}`}
+            href={`/beer/${beerItem._id}`}
           />
         </div>
       </article>
@@ -25,10 +25,7 @@ const Card = (props) => {
 };
 
 Card.propTypes = {
-  name: PropTypes.string,
-  image_url: PropTypes.string,
-  tagline: PropTypes.string,
-  _id: PropTypes.string,
+  beerItem: PropTypes.object,
 };
 
 export default Card;
--- src/pages/BeerList.jsx
+++ src/pages/BeerList.jsx
@@ -1,40 +1,47 @@
-// import { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 import "./BeerList.scss";
 import Navigation from "../components/Navigation/Navigation";
 import Card from "../components/Card/Card";
 
+const url = "https://ih-beers-api2.herokuapp.com/beers";
+
 const BeerList = () => {
-  // const [count, setCount] = useState(0);
-  // useEffect(() => {
-  //   console.log(count);0
-  // }, [count]);
+  const [beerData, setBeerData] = useState();
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setBeerData(data);
+      })
+      .catch((error) => {
+        console.error("Error Message ❌🍺❌ ", error);
+      });
+  }, []);
+
+  if (!beerData) {
+    return (
+      <div className="animation">
+        <p>
+          L <span className="ani">🍺</span> O <span className="ani">🍺</span> A
+          <span className="ani">🍺</span> D <span className="ani">🍺</span> I
+          <span className="ani">🍺</span> N <span className="ani">🍺</span> G
+          <span className="ani">🍺</span> ...
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
-      <h1>BeerList</h1>
       <section>
-        <Card />
-        {/* <article>
-          <h2>{props.property}</h2>
-          <button
-            onClick={() => {
-              setCount(count + 1);
-            }}
-          >
-            click +1
-          </button>
-          <p>{count}</p>
-          <Link to="/">See More</Link>
-        </article> */}
+        {beerData.map((beer) => (
+          <Card key={beer._id} beerItem={beer} />
+        ))}
       </section>
       <Navigation />
     </>
   );
 };
-
-// BeerList.propTypes = {
-//   property: PropTypes.string,
-// };
 
 export default BeerList;
