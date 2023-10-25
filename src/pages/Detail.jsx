176e5e50ecf71ec41f65f389e3310af8b0414e49
SuperCoder <propscode254@gmail.com>
2023-10-25 09:55:31 +0200

    add components

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./Detail.scss";
+
+const Detail = (props) => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  return (
+    <>
+      <h1>Detail</h1>
+      <section>
+        <article>
+          <h2>{props.property}</h2>
+          <button
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            click +1
+          </button>
+          <p>{count}</p>
+          <Link to="/">See More</Link>
+        </article>
+      </section>
+    </>
+  );
+};
+
+Detail.propTypes = {
+  property: PropTypes.string,
+};
+
+export default Detail;
